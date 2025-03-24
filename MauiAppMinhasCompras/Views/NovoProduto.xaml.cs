9e8e047169f8ea1eb8448a31962739b40db8ffb4
marcelogalerani <marcelo.galerani@etec.sp.gov.br>
2025-03-24 18:58:57 -0300

    Ajustes finais no aplicativo e melhorias no front end. Bem como testes no simulador android.

--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -22,6 +22,7 @@ public partial class NovoProduto : ContentPage
 
 			await App.Db.Insert(p);
 			await DisplayAlert("Sucesso", "Registro inserido", "Ok");
+			await Navigation.PopAsync();
 
 		}catch(Exception ex)
 		{
